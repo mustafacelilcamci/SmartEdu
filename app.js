c70b86f4fb1b1a2eab01ccf8666952876e4e3b54
Mustafa Celil Çamcı <mustafacelilc06@gmail.com>
2025-07-23 10:43:09 +0300

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "ejs");  
+
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
 });
 const port = 3000;
 
